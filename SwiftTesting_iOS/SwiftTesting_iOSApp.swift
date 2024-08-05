2e88d8df39f22677020ad1fbb535bae553138f82
HongzCloud <jihong21511@gmail.com>
2024-08-05 22:34:58 +0900

    Initial Commit

---
+++ SwiftTesting_iOS/SwiftTesting_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTesting_iOSApp.swift
+//  SwiftTesting_iOS
+//
+//  Created by 오킹 on 8/5/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTesting_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftTesting_iOSTests/SwiftTesting_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTesting_iOSTests.swift
+//  SwiftTesting_iOSTests
+//
+//  Created by 오킹 on 8/5/24.
+//
+
+import Testing
+@testable import SwiftTesting_iOS
+
+struct SwiftTesting_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
